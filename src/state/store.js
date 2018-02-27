beb581676e2e2539189face95eabe3d53f8cb68f
Nick Fireman <nfireman1@gmail.com>
2018-02-27 00:20:36 -0500

    add `redux-localstorage`

--- src/state/store.js
+++ src/state/store.js
@@ -1,17 +1,20 @@
 import { routerMiddleware } from 'react-router-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import persistState from 'redux-localstorage';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-export default ({ history }) => (
-  createStore(
+export default ({ history }) => {
+  const middleware = applyMiddleware(
+    thunk,
+    routerMiddleware(history),
+    logger,
+  );
+
+  return createStore(
     reducer,
-    applyMiddleware(
-      thunk,
-      routerMiddleware(history),
-      logger,
-    )
-  )
-);
+    compose(middleware, persistState()),
+  );
+};
