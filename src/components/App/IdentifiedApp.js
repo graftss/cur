5d10e89f718948e7a86bb1e4c9e14b0d6866287e
Nick Fireman <nfireman1@gmail.com>
2018-02-27 03:38:43 -0500

    add `NewAppraisalRoute`

--- src/components/App/IdentifiedApp.js
+++ src/components/App/IdentifiedApp.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import Navbar from '../Navbar';
+import NewAppraisalRoute from '../NewAppraisalRoute';
 
 export default () => (
   <div className="App">
     <Navbar />
+    <div className="App-content">
+      <Route path="/new" component={NewAppraisalRoute} />
+    </div>
   </div>
 );
